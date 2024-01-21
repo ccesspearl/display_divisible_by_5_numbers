3dc4a63b6bf5f379791108038c189d91086e420e
ccesspearl <princesspearldelarosa05@gmail.com>
2024-01-22 06:27:34 +0800

    pseudocode

--- display_divisible_by_5_numbers.py
+++ display_divisible_by_5_numbers.py
@@ -1,2 +1,8 @@
 # Exercise 6: Display numbers divisible by 5 from a list
 # Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# Pseudocode 
+
+# Create a variable for given list 
+# Use for loop 
+# Use if statement and print the results 
